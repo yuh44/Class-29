b0e85d39feb5cc48d618113fbac78e630a823983
yuh44 <73146851+yuh44@users.noreply.github.com>
2021-01-07 20:46:49 -0500

    Add files via upload

---
+++ AngryBirdsStage4-master/AngryBirdsStage4-master/Ground.js
@@ -0,0 +1,17 @@
+class Ground {
+    constructor(x,y,width,height) {
+      var options = {
+          isStatic: true
+      }
+      this.body = Bodies.rectangle(x,y,width,height,options);//1
+      this.width = width;
+      this.height = height;
+      World.add(world, this.body);//2
+    }
+    display(){
+      var pos =this.body.position;
+      rectMode(CENTER);
+      fill("brown");
+      rect(pos.x, pos.y, this.width, this.height);//3
+    }
+  };
